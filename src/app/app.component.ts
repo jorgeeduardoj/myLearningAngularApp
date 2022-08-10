5a50a247fbbb3714e61ec398cdf207d02d686007
jorgeeduardo <jorgeeduardo@inspyra.co>
2022-08-09 20:58:41 -0500

    starting a demo project to learn Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myLearningApp';
+  name : string = 'Jorge Eduardo';
 }
